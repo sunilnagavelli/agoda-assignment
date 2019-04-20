f6ff03af9ec224db889892366f4f79fdcb488d80
Sunil <40353788+sunilnagavelli@users.noreply.github.com>
2019-04-20 11:11:39 +0530

    Final commit for the submission

--- FriendsOfFriends/FriendsOfFriends.py
+++ FriendsOfFriends/FriendsOfFriends.py
@@ -2,20 +2,21 @@
 from os import system
 #system('clear')
 
-# FUNCTION FOR UNIQUE FRIENDS IN LISTS
-def friends_of_friends(person,person_friends,all_friends):
-  suggestions=[]
-  for friends in all_friends[person]:
-    if friends in all_friends.keys():
-      for friend in all_friends[friends]:
-        suggestions.append(friend)
-  for items in range(len(suggestions)):
-    if person in suggestions:
-      suggestions.pop(suggestions.index(person))
-    if friend in person_friends:
-      if friend in suggestions:
-        suggestions.pop(suggestions.index(friend))
-  return suggestions
+class Person():
+  # FUNCTION FOR UNIQUE FRIENDS IN LISTS
+  def friends_of_friends(self,person,person_friends,all_friends):
+    suggestions=[]
+    for friends in all_friends[person]:
+      if friends in all_friends.keys():
+        for friend in all_friends[friends]:
+          suggestions.append(friend)
+    for items in range(len(suggestions)):
+      if person in suggestions:
+        suggestions.pop(suggestions.index(person))
+      if friend in person_friends:
+        if friend in suggestions:
+          suggestions.pop(suggestions.index(friend))
+    return suggestions
 
 # MAIN FUNCTION
 if __name__ == '__main__':
@@ -29,8 +30,9 @@ if __name__ == '__main__':
   all_friends['A']=[value.strip(' ') for value in A]
   all_friends['B']=[value.strip(' ') for value in B]
   all_friends['C']=[value.strip(' ') for value in C]
-
+  
+  person_obj = Person()
   for person in all_friends.keys():
     print('\nFriend suggestions for {}: '.format(person), end='')
-    suggestions=friends_of_friends(person,all_friends[person],all_friends)
+    suggestions=person_obj.friends_of_friends(person,all_friends[person],all_friends)
     print(suggestions)
